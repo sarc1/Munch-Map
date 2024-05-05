c8a011c12c2ef05180515a53f1e26d87ba1aae2e
moriel <morielbien101@gmail.com>
2024-05-05 22:09:53 +0800

    sql connection

---
+++ src/main/java/com/example/munch_map/MySQLConnection.java
@@ -0,0 +1,26 @@
+package com.example.munch_map;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+    public static final String URL = "jdbc:mysql://localhost:3306/anything";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        Connection c = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        Connection c = getConnection();
+    }
+}
