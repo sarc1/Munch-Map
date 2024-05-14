c563de11487385a1b95ef0681631d813abd1fedc
rei <philippeandreid@gmail.com>
2024-05-15 00:49:56 +0800

    feat: working login, signup, barangay

--- src/main/java/com/example/munch_map/TableInstantiation.java
+++ src/main/java/com/example/munch_map/TableInstantiation.java
@@ -9,16 +9,26 @@ public class TableInstantiation {
     public static void initializeTables() {
 
         try (Connection c = MySQLConnection.getConnection();
-            Statement statement = c.createStatement()) {
+             Statement statement = c.createStatement()) {
 
-            String query = "CREATE TABLE IF NOT EXISTS tblAccount(" +
-                "acc_id INT AUTO_INCREMENT PRIMARY KEY," +
-                "username VARCHAR(100) NOT NULL," +
-                "email VARCHAR(100) NOT NULL," +
-                "password VARCHAR(100) NOT NULL)";
-            statement.execute(query);
+            // Table Queries
+            String createAccountTableQuery = "CREATE TABLE IF NOT EXISTS tblAccount(" +
+                    "acc_id INT AUTO_INCREMENT PRIMARY KEY," +
+                    "username VARCHAR(100) NOT NULL," +
+                    "email VARCHAR(100) NOT NULL," +
+                    "password VARCHAR(100) NOT NULL)";
+
+            String createBarangayTableQuery = "CREATE TABLE IF NOT EXISTS tblBarangay(" +
+                    "barangay_name VARCHAR(100) NOT NULL PRIMARY KEY)";
+            //TOD0: insert Location into tblBarangay (Coordinates)
+
+            // Query Executions
+            statement.execute(createAccountTableQuery);
+            statement.execute(createBarangayTableQuery);
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 }
