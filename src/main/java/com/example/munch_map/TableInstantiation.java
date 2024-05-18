a409f83a75c201b0fd3744d2e16b27d4566c70f2
Kiyokun <yoshinori.kyonojr@cit.edu>
2024-05-18 16:16:40 +0800

    Added Table Review, and modified viewPlaces.fxml

--- src/main/java/com/example/munch_map/TableInstantiation.java
+++ src/main/java/com/example/munch_map/TableInstantiation.java
@@ -27,12 +27,24 @@ public class TableInstantiation {
                     "place_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
                     "barangay_id INT(10) NOT NULL, " +
                     "place_name VARCHAR(255) NOT NULL, " +
+                    "place_type VARCHAR(255) NOT NULL," +
                     "FOREIGN KEY (barangay_id) REFERENCES tblBarangay(barangay_id) ON DELETE CASCADE)";
+
+            String createReviewTableQuery = "CREATE TABLE IF NOT EXISTS tblReviews (" +
+                    "review_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+                    "place_id INT(10) NOT NULL," +
+                    "acc_id INT NOT NULL," +
+                    "rating DECIMAL(2,1) NOT NULL CHECK (rating >= 0.0 AND rating <= 5.0)," +
+                    "comment VARCHAR(255)," +
+                    "FOREIGN KEY (place_id) REFERENCES tblPlace(place_id) ON DELETE CASCADE," +
+                    "FOREIGN KEY (acc_id) REFERENCES tblAccount(acc_id) ON DELETE CASCADE," +
+                    "isApproved TINYINT(1) NOT NULL DEFAULT 0)";
 
             // Query Executions
             statement.execute(createAccountTableQuery);
             statement.execute(createBarangayTableQuery);
             statement.execute(createPlaceTableQuery);
+            statement.execute(createReviewTableQuery);
 
         } catch (SQLException e) {
             e.printStackTrace();
