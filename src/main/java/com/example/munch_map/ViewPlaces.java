b1083e23b29c91d6299c94528c3a2c77ddf2d0f4
rei <philippeandreid@gmail.com>
2024-05-19 20:48:38 +0800

    feat: add service and task to view to increase performance

--- src/main/java/com/example/munch_map/ViewPlaces.java
+++ src/main/java/com/example/munch_map/ViewPlaces.java
@@ -2,129 +2,123 @@ package com.example.munch_map;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Connection;
 
 public class ViewPlaces {
 
     public AnchorPane viewPlacesAnchorPane;
     public ListView<String> placeListView;
     public Label barangayNameLabel;
-    public ObservableList<String> places;
+    public ObservableList<String> places = FXCollections.observableArrayList();
 
     public Label placeName;
     public Label placeRating;
     public Label placeType;
 
     public void initialize() {
-        barangayNameLabel.setText(Barangay.selectedBarangay);
-        places = FXCollections.observableArrayList();
+        updateBarangayLabel();
+        setupListView();
+    }
 
-        Task<ObservableList<String>> task = new FetchPlacesTask();
-        placeListView.itemsProperty().bind(task.valueProperty());
-
-        placeListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            // 'newValue' contains the selected place name. You can use 'newValue' to fetch ratings from your database.
-            String selectedPlaceName = newValue;
-
-            if (selectedPlaceName != null) {
-                updatePlaceDetails(selectedPlaceName);
+    private void updateBarangayLabel() {
+        Service<Void> updateLabelService = new Service<>() {
+            @Override
+            protected Task<Void> createTask() {
+                return new Task<>() {
+                    @Override
+                    protected Void call() {
+                        barangayNameLabel.setText(Barangay.selectedBarangay);
+                        return null;
+                    }
+                };
             }
-        });
-
-        new Thread(task).start();
+        };
+        updateLabelService.start();
     }
 
-    private void updatePlaceDetails(String selectedPlaceName) {
-        // Use selectedPlaceName to fetch data from your database and update 'placeName' and 'placeRatings' labels.
+    private void setupListView() {
+        Service<Void> setupListViewService = new Service<>() {
+            @Override
+            protected Task<Void> createTask() {
+                return new Task<>() {
+                    @Override
+                    protected Void call() {
+                        placeListView.itemsProperty().bind(new FetchPlacesTask().valueProperty());
+                        placeListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+                            if (newValue != null) {
+                                updatePlaceDetails(newValue);
+                            }
+                        });
 
-        // Assume 'getPlaceNameFromDB' and 'getPlaceRatingsFromDB' are your methods to fetch data from the database.
-        placeName.setText(selectedPlaceName); // Set the new placeName to the Label.
-        placeRating.setText(getPlaceRatingFromDB(selectedPlaceName)); // Set the new placeRating to the Label.
-        placeType.setText(getPlaceTypeFromDB(selectedPlaceName));
+                        new Thread(new FetchPlacesTask()).start();
+
+                        return null;
+                    }
+                };
+            }
+        };
+        setupListViewService.start();
     }
 
-    private String getPlaceRatingFromDB(String placeName) {
-        // This function should go to the DB and retrieve the average rating for a provided placeName.
+    private void updatePlaceDetails(String name) {
+        Service<Void> updatePlaceDetailService = new Service<>() {
+            @Override
+            protected Task<Void> createTask() {
+                return new Task<>() {
+                    @Override
+                    protected Void call() {
+                        placeName.setText(name);
+                        placeRating.setText(retrieveDataFromDB("AVG(r.rating)", "avg_rating", name));
+                        placeType.setText(retrieveDataFromDB("p.place_type", "place_type", name));
+                        return null;
+                    }
+                };
+            }
+        };
+        updatePlaceDetailService.start();
+    }
 
+    private String retrieveDataFromDB(String selectedData, String columnName, String placeName) {
+        final String query = String.format("SELECT %s FROM tblReviews r INNER JOIN tblPlace p ON r.place_id = p.place_id WHERE p.place_name = ?", selectedData);
         try (Connection c = MySQLConnection.ds.getConnection();
-             PreparedStatement statement = c.prepareStatement(
-                     "SELECT AVG(r.rating) as avg_rating " +
-                             "FROM tblReviews r " +
-                             "INNER JOIN tblPlace p ON r.place_id = p.place_id " +
-                             "WHERE p.place_name = ?;"
-             )) {
-
+             PreparedStatement statement = c.prepareStatement(query)) {
             statement.setString(1, placeName);
             ResultSet rs = statement.executeQuery();
 
-            if(rs.next()) {
-                return rs.getString("avg_rating");
+            if (rs.next()) {
+                return rs.getString(columnName);
             }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
         }
-
-        return "N/A"; // Return "Not Available" if place has no rating yet.
-    }
-
-    private String getPlaceTypeFromDB(String placeName) {
-        // This function should go to the DB and retrieve the place type for a provided placeName.
-
-        try (Connection c = MySQLConnection.ds.getConnection();
-             PreparedStatement statement = c.prepareStatement(
-                     "SELECT p.place_type " +
-                             "FROM tblPlace p " +
-                             "WHERE p.place_name = ?;"
-             )) {
-
-            statement.setString(1, placeName);
-            ResultSet rs = statement.executeQuery();
-
-            if(rs.next()) {
-                return rs.getString("place_type");
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return "N/A"; // Return "Not Available" if place type is not found.
+        return "N/A";
     }
 
     class FetchPlacesTask extends Task<ObservableList<String>> {
         @Override
         protected ObservableList<String> call() {
+            final String fetchPlacesQuery = "SELECT p.place_id, p.place_name FROM tblPlace p INNER JOIN tblBarangay b ON p.barangay_id = b.barangay_id WHERE b.barangay_name = ?";
             try (Connection c = MySQLConnection.ds.getConnection();
-                 PreparedStatement statement = c.prepareStatement(
-                         "SELECT p.place_id, p.place_name " +
-                                 "FROM tblPlace p " +
-                                 "INNER JOIN tblBarangay b ON p.barangay_id = b.barangay_id " +
-                                 "WHERE b.barangay_name = ?;"
-                 )) {
-
+                 PreparedStatement statement = c.prepareStatement(fetchPlacesQuery)) {
                 statement.setString(1, Barangay.selectedBarangay);
                 ResultSet rs = statement.executeQuery();
 
-                while(rs.next()) {
+                while (rs.next()) {
                     places.add(rs.getString("place_name"));
                 }
-
-            } catch (SQLException e) {
-                e.printStackTrace();
+            } catch (SQLException exception) {
+                exception.printStackTrace();
             }
-
             return places;
         }
-
-
     }
 }
