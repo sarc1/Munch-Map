a5b49528d9e53db8cf39854320d4605341cd4a55
rei <philippeandreid@gmail.com>
2024-05-30 07:43:18 +0800

    feat: back buttons

--- src/main/java/com/example/munch_map/Barangay.java
+++ src/main/java/com/example/munch_map/Barangay.java
@@ -5,15 +5,11 @@ import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.Connection;
