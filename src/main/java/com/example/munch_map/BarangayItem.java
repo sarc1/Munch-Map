d1088ceb2b78008923c2df10d064f585d0427839
rei <philippeandreid@gmail.com>
2024-05-29 21:17:55 +0800

    feat: barangay implementation

--- src/main/java/com/example/munch_map/BarangayItem.java
+++ src/main/java/com/example/munch_map/BarangayItem.java
@@ -1,4 +1,18 @@
 package com.example.munch_map;
 
 public class BarangayItem {
+    private String name;
+
+    public BarangayItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
