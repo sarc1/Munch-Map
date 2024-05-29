3bfbbccd1c3cf493da6463f6627c40261c82b182
arce <arcelyn.tequillo0220@gmail.com>
2024-05-29 23:26:43 +0800

    fix: view places css

--- src/main/java/com/example/munch_map/MunchMap.java
+++ src/main/java/com/example/munch_map/MunchMap.java
@@ -8,6 +8,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
@@ -20,8 +21,31 @@ public class MunchMap {
 
     @FXML
     AnchorPane LoginSignupPage;
+    @FXML
     Text lblMunchMap;
+    @FXML
+    Button btnLogin;
+    @FXML
+    Button btnSignup;
 
+    @FXML
+    public void initialize() {
+        lblMunchMap.setOpacity(0.0);
+        btnSignup.setOpacity(0.0);
+        btnLogin.setOpacity(0.0);
+        FadeTransition fadeTransition = new FadeTransition(Duration.millis(2500), lblMunchMap);
+        fadeTransition.setFromValue(0.0);
+        fadeTransition.setToValue(1.0);
+        FadeTransition fadeTransition1 = new FadeTransition(Duration.millis(2500), btnSignup);
+        fadeTransition1.setFromValue(0.0);
+        fadeTransition1.setToValue(1.0);
+        FadeTransition fadeTransition2 = new FadeTransition(Duration.millis(2500), btnLogin);
+        fadeTransition2.setFromValue(0.0);
+        fadeTransition2.setToValue(1.0);
+        fadeTransition.play();
+        fadeTransition1.play();
+        fadeTransition2.play();
+    }
     @FXML
     public void onLoginButtonClick() throws IOException {
         AnchorPane p = LoginSignupPage;
@@ -32,10 +56,11 @@ public class MunchMap {
         loading.getStylesheets().add(Objects.requireNonNull(getClass().getResource("transition.css")).toExternalForm());
         p.getChildren().add(loading);
 
+
+
         new Thread(() -> {
             try {
                 Parent scene = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
-
                 Platform.runLater(() -> {
                     p.getScene().getStylesheets().clear();
                     p.getScene().getStylesheets().add(Objects.requireNonNull(getClass().getResource("login.css")).toExternalForm());
